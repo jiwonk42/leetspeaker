49f94ea6c93ef1bd6278fe3a6708a728ad9a2e6a
Jiwon Kang and Renee Mei <jiwon+renee@>
2017-02-13 13:50:32 -0800

    add scripts to meet spec 2,3 and 4

--- Objects/LeetspeakerTranslate.cs
+++ Objects/LeetspeakerTranslate.cs
@@ -7,7 +7,24 @@ namespace Leetspeaker
   {
     public string Translate(string input)
     {
-      return input;
+      char[] array = input.ToCharArray();
+      for (int i = 0; i < array.Length; i++)
+      {
+        if (array[i] == 'e'|| array[i] == 'E')
+        {
+          array[i] = (3).ToString().ToCharArray()[0];
+        }
+        else if (array[i] == 'o'|| array[i] == 'O')
+        {
+          array[i] = (0).ToString().ToCharArray()[0];
+        }
+        else if (array[i] == 'I')
+        {
+          array[i] = (1).ToString().ToCharArray()[0];
+        }
+      }
+      string result = string.Join("", array);
+      return result;
     }
   }
 }
--- Tests/LeetspeakerTranslateTest.cs
+++ Tests/LeetspeakerTranslateTest.cs
@@ -12,5 +12,26 @@ namespace Leetspeaker
       LeetspeakerTranslator testLeetspeaker = new LeetspeakerTranslator();
       Assert.Equal("a", testLeetspeaker.Translate("a"));
     }
+
+    [Fact]
+    public void LeetspeakerTranslator_ForUserInpute_3()
+    {
+      LeetspeakerTranslator testLeetspeaker = new LeetspeakerTranslator();
+      Assert.Equal("3gg", testLeetspeaker.Translate("Egg"));
+    }
+
+    [Fact]
+    public void LeetspeakerTranslator_ForUserInputo_0()
+    {
+      LeetspeakerTranslator testLeetspeaker = new LeetspeakerTranslator();
+      Assert.Equal("n30n", testLeetspeaker.Translate("neon"));
+    }
+
+    [Fact]
+    public void LeetspeakerTranslator_ForUserInputI_1()
+    {
+      LeetspeakerTranslator testLeetspeaker = new LeetspeakerTranslator();
+      Assert.Equal("1'm L30", testLeetspeaker.Translate("I'm Leo"));
+    }
   }
 }
